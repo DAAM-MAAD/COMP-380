c505b2a78ec1fe6b1c00ff531f93a96eab348b88
AB <abpaxtor@gmail.com>
2023-03-08 18:40:07 -0800

    Scalable Boxes for GUI and Login/Exit

--- Prototype Folder/gui.java
+++ Prototype Folder/gui.java
@@ -2,23 +2,34 @@ import javax.swing.*;
 import java.awt.*;
 
 public class gui {
-
+    public static final int PADDING = 50;
     gui() {
+        // Creating windows frame, titling it, asnd making it visible
         JFrame main = new JFrame("MAAD");
         Dimension preferred = main.getPreferredSize();
         main.setPreferredSize(preferred);
-        main.setLayout(null);
-        main.setVisible(true);
+        main.setMinimumSize(new Dimension(500, 300));
+        //main.setLayout(null);
+
+
+
+
+//        //Creates Buttons
+//        main.setAlwaysOnTop(false);
+//        JButton Reserve = new JButton("Reserve Room");
+//        Reserve.setBounds(0, 20, 150, 40);
+
+        // PANEL
+        JPanel mainMenu = new JPanel(new GridLayout(0,1,PADDING, PADDING));
+        mainMenu.setBorder(BorderFactory.createEmptyBorder(PADDING, PADDING, PADDING, PADDING));
+        mainMenu.add( new JButton("Login"));
+        mainMenu.add( new JButton("Exit"));
+        main.add(mainMenu, BorderLayout.CENTER);
 
         main.setLocationRelativeTo(null);
         main.pack();
+        main.setVisible(true);
 
-        main.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
-        int height = java.awt.Frame.MAXIMIZED_VERT;
-        int width = java.awt.Frame.MAXIMIZED_HORIZ;
-        JButton Reserve = new JButton("Reserve Room");
-        Reserve.setBounds(width / 4, height / 4, 150, 40);
-        main.add(Reserve);
 
     }
 
