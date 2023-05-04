977da05e80bf0e1f9fe68a33bdda501f2ca34c67
derick maradiaga <maradiaga.derick@gmail.com>
2023-05-03 18:40:26 -0700

    added imports

--- Prototype Folder/DatabaseH.java
+++ Prototype Folder/DatabaseH.java
@@ -13,6 +13,14 @@ import java.util.*;
 // import javax.mail.internet.*;
 // import javax.activation.*;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 /**
  * Represents a Database
  * @name
